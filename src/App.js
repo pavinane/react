c27464ebcaccabc0381f64b0cf4ad9e43a7aa40e
pavinane <pavimegan77@gmail.com>
2019-08-14 13:44:26 +0530

    Arrow Function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function App() {
-  return (
-   <p>Hello</p>
-  );
-}
+// function App() {
+//   return (
+//    <p>Hello</p>
+//   );
+// }
+
+//Arrow Function
+
+const App = ()=>(<p>hello</p>)
+
+
 export default App;
